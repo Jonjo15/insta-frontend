ac38b4060eb824a8f00e9726ff66216d2265c037
IVAN <ivanbatur5g@gmail.com>
2021-02-10 14:31:12 +0100

    Removed boilerplate and added dependencies

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Route, Redirect} from "react-router-dom"
+// import { useAuth } from '../context/authContext'
+export default function PrivateRoute({ component: Component, ...rest }) {
+    //TODO: GET USER FROM AUTH PROVIDDER
+    // const {state: {currentUser}} = useAuth()
+    return (
+        <Route
+        {...rest}
+        render={props => {
+            // return currentUser ? <Component {...props} /> : <Redirect to="/" />
+        }}
+    ></Route>
+    )
+}
