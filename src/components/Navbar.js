fa826c025ce4d898324a6d48d8b1573d34ff73fd
IVAN <ivanbatur5g@gmail.com>
2021-02-16 22:14:38 +0100

    added some stuff with label

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import {Container, Menu, Dropdown, Button} from "semantic-ui-react"
+import {Container, Menu, Dropdown, Button, Label, Icon} from "semantic-ui-react"
 import { useAuth } from '../context/auth/AuthContext'
 import { useNotifications } from '../context/notifications/NotificationsContext'
 export default function Navbar() {
@@ -29,14 +29,16 @@ export default function Navbar() {
                         <Dropdown.Item text="You have no follow requests"/>
                     </Dropdown.Menu>
                 </Dropdown>
-                <Dropdown icon="alarm">
-                    <Dropdown.Menu>
-                        {/* TODO: REFACTOR NOTIFICATIONS INTO ITS OWN COMPONENTS */}
-                        {notifications?.length === 0 && <Dropdown.Item text="You have no notifications"/>}
-                        {notifications?.length > 0 && notifications.map(n => <Dropdown.Item as={Link} to={"/posts/" + n.postId} key={n._id} text={n.sender.username} />)}
-                        {notifications.filter(n => n.seen === false).length > 0 && <Dropdown.Item icon="trash" as={Button} text="Mark all notifications read" onClick={markAllRead}/>}
-                    </Dropdown.Menu>
-                </Dropdown>
+                
+                    <Dropdown icon="alarm">
+                        <Dropdown.Menu>
+                            {/* TODO: REFACTOR NOTIFICATIONS INTO ITS OWN COMPONENTS */}
+                            {notifications?.length === 0 && <Dropdown.Item text="You have no notifications"/>}
+                            {notifications?.length > 0 && notifications.map(n => <Dropdown.Item as={Link} to={"/posts/" + n.postId} key={n._id} text={n.sender.username} />)}
+                            {notifications.filter(n => n.seen === false).length > 0 && <Dropdown.Item icon="trash" as={Button} text="Mark all notifications read" onClick={markAllRead}/>}
+                        </Dropdown.Menu>{notifications.filter(n => n.seen === false).length}
+                    </Dropdown>
+                
                 <Dropdown icon="user">
                     <Dropdown.Menu>
                         <Dropdown.Item icon="user" text="Profile" as={Link} to="/requests"/>
