8adcf4a64f4b5b6614c01327991e64ab42668aeb
IVAN <ivanbatur5g@gmail.com>
2021-03-04 22:01:03 +0100

    Added a form for comments

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Modal, Image, Card } from "semantic-ui-react"
+import React, { useState } from 'react'
+import { Modal, Image, Card, Button } from "semantic-ui-react"
 import {Link} from "react-router-dom"
 import dayjs from "dayjs"
 
@@ -7,6 +7,11 @@ var relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
 export default function PostModal({post, open, setOpen}) {
     // const [open, setOpen] = useState(false)
+    const [body, setBody] = useState("")
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log("submit")
+    }
     return (
             <Modal
             onClose={() => setOpen(false)}
@@ -23,12 +28,35 @@ export default function PostModal({post, open, setOpen}) {
                     <div className="modal-right">
                         <Card fluid>
                             <Card.Content>
-                                
+                                <Image
+                                circular
+                                floated='left'
+                                size='mini'
+                                src={post.poster.profile_pic_url}
+                                />
                                 <Card.Header style={{marginTop: 5, marginLeft: 5}}><Link to={"/users/" + post.poster._id}>{post.poster.username}</Link></Card.Header>
                             </Card.Content>
                         </Card>
                         {/* TODO: REPLACE BELOW WITH CUSTOM HTML THIS IS GARBAGE */}
-                        <Card fluid>
+                        <div className="comments">
+                            <div className="comment-card">
+                                <p><Link to={"/users/" + post.poster._id}>{post.poster.username}</Link> {post.body}</p>
+                            </div>
+                            {post.comments.map(c => <div className="comment-card">
+                                <p><Link to={"/users/" + c.commenter._id}>{c.commenter.username}</Link> {c.body}</p>
+                            </div> )}
+                            {post.comments.length === 0 && <p className="center">No comments yet</p>}
+                        </div>
+                        <div className="modal-post-form">
+                            <div className="w-100">
+
+                                <form className="comment-form" onSubmit={handleSubmit}>
+                                    <input placeholder="Post a comment" type="text" onChange={(e) => setBody(e.target.value)} value={body}/>
+                                    <Button type="submit" content="Submit"/>
+                                </form>
+                            </div>
+                        </div>
+                        {/* <Card fluid>
                             <Card.Content>
                                 <Card.Description>
                                     <Image
@@ -43,7 +71,7 @@ export default function PostModal({post, open, setOpen}) {
                                     <span className='date'>{dayjs(post.createdAt).fromNow()}</span>
                                 </Card.Meta>
                             </Card.Content>
-                        </Card>
+                        </Card> */}
                     </div>
                 </div>
             </Modal>
